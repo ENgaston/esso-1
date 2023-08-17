9bfaff4e31d35e6ba3fcd2b9af7b1b17bab6db11
ENgaston <essomenkouegaston@gmail.com>
2023-08-16 23:12:58 -0500

    test16

---
+++ script5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check for the required argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 esso"
+    exit 1
+fi
+
+# Check the user in /etc/passwd
+if grep -q "^$1:" /etc/passwd; then
+    echo "User $1 exists."
+else
+    echo "User $1 does not exist."
